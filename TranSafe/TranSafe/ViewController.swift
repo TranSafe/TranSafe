477e5d21ed094f0238b7fd8619ba931ce2f248f0
Kevin Asistores <kasistores@ufl.edu>
2016-10-08 04:02:22 -0400

    fix

--- TranSafe/TranSafe/ViewController.swift
+++ TranSafe/TranSafe/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import DTMHeatmap
+import MapKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.locationManager.delegate = self
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+//        self.mapView.delegate = self
+//        // Create DTMHeatmap
+//        self.heatmap = DTMHeatmap()
+//        self.heatmap.data = myData
+//        self.mapView.addOverlay(self.heatmap)
+//        // or...
+//        // Create DTMDiffHeatmap
+//        self.diffHeatmap = DTMDiffHeatmap()
+//        self.diffHeatmap.setBeforeData(beforeData, afterData: afterData)
+//        self.mapView.addOverlay(self.diffHeatmap)
+//    }
+//    
+//    func mapView(mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+//        return DTMHeatmapRenderer(overlay)
     }
 
     override func didReceiveMemoryWarning() {
